4ad7ff857a1285181775e14a831d6346268453d1
Aleksandre-jachvadze <aleksandrejachvi@gmail.com>
2024-11-27 20:35:13 +0400

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        "pale-blue": "hsl(225, 100%, 94%)",
+        "bright-blue": "hsl(245, 75%, 52%)",
+        "very-pale-blue": "hsl(225, 100%, 98%)",
+        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "dark-blue": "hsl(223, 47%, 23%)",
+      },
+      colors: {
+        "pale-blue": "hsl(225, 100%, 94%)",
+        "bright-blue": "hsl(245, 75%, 52%)",
+        "very-pale-blue": "hsl(225, 100%, 98%)",
+        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "dark-blue": "hsl(223, 47%, 23%)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
